8adf1e7872b9fcbcedd7a557d24257dad10db4a3
Michael Yanovich <michael@yanovich.net>
2012-08-24 18:57:21 -0400

    re-compile rxvt with 256 colour support

--- fix-rxvt.sh
+++ fix-rxvt.sh
@@ -10,6 +10,8 @@ cd rxvt-unicode-*/
 
 perl -pi -e 's/--enable-iso14755/--disable-iso14755/g' debian/rules
 
+patch -p1 < doc/urxvt-8.2-256color.patch
+
 fakeroot debian/rules binary
 
 sudo dpkg -i ../rxvt-unicode-ml*deb
